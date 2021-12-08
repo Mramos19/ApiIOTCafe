1c0d4f29291321b7200e356b85e109f2431a3fda
Martin Ramos <martinramoso@hotmail.com>
2021-12-08 17:51:57 -0600

    Integracion de clase connexion

--- ApiIOTCafe.Service/User/IUserServices.cs
+++ ApiIOTCafe.Service/User/IUserServices.cs
@@ -13,5 +13,9 @@ namespace ApiIOTCafe.Service.User
     public interface IUserServices
     {
         public Task<LoginResponse> Login(LoginRequest Request);
+        public List<UserEntities> UserGetAll();
+        public Task<int> Insert(UserEntities Request);
+        public Task<int> Update(UserEntities Request);
+        public Task<int> Delete(UserEntities Request);
     }
 }
--- ApiIOTCafe/Controllers/UserController.cs
+++ ApiIOTCafe/Controllers/UserController.cs
@@ -5,7 +5,7 @@ using ApiIOTCafe.Entities;
 using ApiIOTCafe.Model.User.Request;
 using ApiIOTCafe.Model.User.Response;
 using ApiIOTCafe.Service.User;
-
+using ApiIOTCafe.Model.User.Entities;
 
 namespace ApiIOTCafe.Controllers
 {
@@ -32,6 +32,102 @@ namespace ApiIOTCafe.Controllers
                 _Response.StatusCode = "00";
                 _Response.Message = "Success";
                 _Response.Result = await _UserServices.Login(_Request);
+
+                return StatusCode(200, _Response);
+
+            }
+            catch (Exception ex)
+            {
+                _Response.StatusCode = "05";
+                _Response.Message = ex.Message;
+                return StatusCode(500, _Response);
+            }
+
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult UserGetAll()
+        {
+
+            ResponseEntities<List<UserEntities>> _Response = new ResponseEntities<List<UserEntities>>();
+
+            try
+            {
+                _Response.StatusCode = "00";
+                _Response.Message = "Success";
+                _Response.Result = _UserServices.UserGetAll();
+
+                return StatusCode(200, _Response);
+
+            }
+            catch (Exception ex)
+            {
+                _Response.StatusCode = "05";
+                _Response.Message = ex.Message;
+                return StatusCode(500, _Response);
+            }
+
+        }
+
+        [HttpPost("Insert")]
+        public async Task<IActionResult> Insert(UserEntities _Request)
+        {
+
+            ResponseEntities<int> _Response = new ResponseEntities<int>();
+
+            try
+            {
+                _Response.StatusCode = "00";
+                _Response.Message = "Success";
+                _Response.Result = await _UserServices.Insert(_Request);
+
+                return StatusCode(200, _Response);
+
+            }
+            catch (Exception ex)
+            {
+                _Response.StatusCode = "05";
+                _Response.Message = ex.Message;
+                return StatusCode(500, _Response);
+            }
+
+        }
+
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update(UserEntities _Request)
+        {
+
+            ResponseEntities<int> _Response = new ResponseEntities<int>();
+
+            try
+            {
+                _Response.StatusCode = "00";
+                _Response.Message = "Success";
+                _Response.Result = await _UserServices.Update(_Request);
+
+                return StatusCode(200, _Response);
+
+            }
+            catch (Exception ex)
+            {
+                _Response.StatusCode = "05";
+                _Response.Message = ex.Message;
+                return StatusCode(500, _Response);
+            }
+
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<IActionResult> Delete(UserEntities _Request)
+        {
+
+            ResponseEntities<int> _Response = new ResponseEntities<int>();
+
+            try
+            {
+                _Response.StatusCode = "00";
+                _Response.Message = "Success";
+                _Response.Result = await _UserServices.Delete(_Request);
 
                 return StatusCode(200, _Response);
 
